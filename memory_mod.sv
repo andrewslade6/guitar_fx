ddad3489d5bdadd35cbe681a539ec6a9a834cabd
SamsoniteProsser <samsoniteprosser@gmail.com>
2017-10-03 17:44:48 -0500

    changed files to Hoff meeting versions

--- memory_mod.sv
+++ memory_mod.sv
@@ -1,32 +1,20 @@
-// Internal RAM model
 module memory_mod(w_en, d_in, d_out, r_addr, w_addr, clk, rst_n);
 
 input wire w_en, clk, rst_n;
 input reg[15:0] d_in;
-input reg[14:0] r_addr, w_addr;
+input reg[15:0] r_addr, w_addr;
 
 output reg[15:0] d_out;
 
 // memory d29280 = h7260 = 0.6 seconds of samples
-reg [15:0] memory [0:29280];
+reg [15:0] memory [48827:0];
 
 // read write logic
-always_ff @(posedge clk or negedge rst_n) begin
-	if(~rst_n) begin
+always_ff @(posedge clk)	begin
+	if(w_en) 
+		memory[w_addr] <= d_in;
+	d_out = memory[r_addr];
 
-		//WHAT DO WE DO?
-	
-	end else
-	begin
-		if(w_en) 
-		begin
-			memory[w_addr] <= d_in;
-		end
-
-	end
 end
-
-assign d_out = memory[r_addr];
-
 
 endmodule 
